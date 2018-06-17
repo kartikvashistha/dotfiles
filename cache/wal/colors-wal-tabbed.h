889a897af748c38a09c7c9a896cd49504d6adb86
Kartik Vashistha <kartikanku@gmail.com>
2018-06-17 15:50:01 +0000

    Deleted cache dir

--- cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0e1b2b";
-static const char* selfgcolor   = "#cebfc1";
-static const char* normbgcolor  = "#DDA16F";
-static const char* normfgcolor  = "#cebfc1";
-static const char* urgbgcolor   = "#C97453";
-static const char* urgfgcolor   = "#cebfc1";
