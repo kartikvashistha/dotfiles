ff719355806f860834c8631c28b4d15e7de7833e
Kartik Vashistha <kartikv.work@gmail.com>
2024-03-03 15:16:58 +0000

    feat: init custom nvim
    init custom nvim by installing lazy & configuring a few additional settings

---
+++ templates/nvim/lua/config/config.lua
@@ -0,0 +1,16 @@
+-- Set colourscheme
+-- vim.cmd[[colorscheme tokyonight]]
+
+-- Enable line number
+vim.opt.relativenumber=false
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
