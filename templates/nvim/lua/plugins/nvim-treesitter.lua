a337a69902c8b66fc04c537e045f5c876e959e69
Kartik Vashistha <kartikv.work@gmail.com>
2024-03-04 00:39:11 +0000

    feat: init treesitter
    this plugin will provide code highlighting

---
+++ templates/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,15 @@
+-- Treesitter, for code highlighting
+return {
+  {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+          local config = require("nvim-treesitter.configs")
+          config.setup({
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+          })
+        end
+    },
+}
