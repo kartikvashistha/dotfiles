14046668bfddb3bd4d79eaa5029fa5df7c538948
Kartik Vashistha <kartikv.work@gmail.com>
2024-03-04 00:41:30 +0000

    style: cleaning up a bit

--- templates/nvim/lua/plugins/nvim-treesitter.lua
+++ templates/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,15 @@
 -- Treesitter, for code highlighting
 return {
-  {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-          local config = require("nvim-treesitter.configs")
-          config.setup({
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-          })
-        end
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "bash", "go", "lua", "markdown", "vim", "vimdoc" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
