810bb348db486cf7682393a66720546669d19269
Kartik Vashistha <kartikv.work@gmail.com>
2024-03-04 00:23:55 +0000

    feat: update config

--- templates/nvim/lua/config/config.lua
+++ templates/nvim/lua/config/config.lua
@@ -1,16 +1,15 @@
 -- Set colourscheme
--- vim.cmd[[colorscheme tokyonight]]
+vim.cmd([[colorscheme tokyonight]])
 
 -- Enable line number
-vim.opt.relativenumber=false
+vim.opt.relativenumber = true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
