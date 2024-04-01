ac7a5b972d966f3a6bbaea023bea5492dfcb5eca
Kartik Vashistha <kartikv.work@gmail.com>
2024-04-02 00:53:12 +0100

    style: change default theme to rose-pine-moon

--- templates/nvim/lua/config/config.lua
+++ templates/nvim/lua/config/config.lua
@@ -1,6 +1,6 @@
 -- Set colourscheme
 vim.cmd("set termguicolors")
-vim.cmd([[colorscheme rose-pine]])
+vim.cmd([[colorscheme rose-pine-moon]])
 
 -- Enable vim options
 vim.opt.number = true
@@ -15,9 +15,13 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
--- Set keymaps
--- Navigate vim panes better.
--- See `:help wincmd` for a list of all window commands
+-- Navigate vim panes better
+-- vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+-- vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+-- vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+-- vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+--  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
